d821476284a4b536b50c704865caf5ae088b8d7f
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2024-11-15 17:27:27 +0000

    Adds checks that the input is a single character

--- milestone_2.py
+++ milestone_2.py
@@ -1,5 +1,19 @@
+# %%
 import random
 
 word_list = ["mango", "blueberries", "strawberries", "banana", "pineapple"]
 word = random.choice(word_list)
 print(word)
+
+# %%
+print("Enter a single letter:")
+
+guess = input()
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+
+else: 
+    print("Oops! That is not a valid input.")
+
+# %%
