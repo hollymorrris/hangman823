6d213542a3646acf7fc93937b64e17639f440f20
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2024-11-16 17:49:46 +0000

    Creates the game class

--- milestone_4.py
+++ milestone_4.py
@@ -2,9 +2,9 @@
 import random
 
 class Hangman:
-	def __init__ (self, word_list, num_lives = 5):
+	def __init__ (self, word_list, num_lives=5):
 		self.word = random.choice(word_list)
-		self.word_guessed = ["_", "_", "_", "_", "_"]
+		self.word_guessed = ["_" for _ in self.word]
 		self.num_letters = len(set(self.word))
 		self.num_lives = num_lives
 		self.word_list = word_list
@@ -14,6 +14,15 @@ class Hangman:
 		guess = guess.lower()
 		if guess in self.word:
 			print("Good guess!", guess, " is in the word.")
+			for i, letter in enumerate(self.word):
+				if letter == guess:
+					self.word_guessed[i] = guess
+			print(self.word_guessed)
+			self.num_letters -= 1
+		else:
+			self.num_lives -= 1
+			print("Sorry,", guess, " is not in the word.")
+			print("You have", self.num_lives, "lives left.")
 	
 	def ask_for_input(self):
 		while True:
@@ -26,6 +35,7 @@ class Hangman:
 			else:
 				self.check_guess(guess)
 				self.list_of_guesses.append(guess)
-	ask_for_input()
 
-# %%
+word_list = ["giraffe", "duvet", "laptop", "car", "painting", "basket", "music"]
+game = Hangman(word_list)
+game.ask_for_input()
