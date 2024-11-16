f7eb09389639a635217d4263405e63b8b3b3bda4
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2024-11-16 13:03:21 +0000

    Creates milestone_3.py and writes script that: checks if the input is a valid gu/ ess and checks whether the guess is in the word.

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,27 @@
-while True:
-	print("Guess a single letter:")
-	guess = input()
-	
-	if len(guess) == 1 and guess.isalpha():
-		break
+# %%
+import random
+
+names_of_favourite_fruit_list = ["mango", "blueberries", "strawberries", "banana", "pineapple"]
+name_of_favourite_fruit = random.choice(names_of_favourite_fruit_list)
+
+def check_guess(guess):
+	guess = guess.lower()
+	if guess in name_of_favourite_fruit:
+		print("Good guess!", guess, "is in the word.")
 	else:
-		print("Invalid letter. Please, enter a single alphabetical character.")
+		print("Sorry,", guess, " is not in the word. Try again.")
+
+def ask_for_input():
+	while True:
+		print("Guess a single letter:")
+		guess = input()
+	
+		if len(guess) == 1 and guess.isalpha():
+			break
+		else:
+			print("Invalid letter. Please, enter a single alphabetical character.")
+			continue 
+	check_guess(guess)
+ask_for_input()
+
 
