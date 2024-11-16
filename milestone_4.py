57e866a97f7bf3f56dff02d825699a90e188c370
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2024-11-16 17:47:59 +0000

    Creates the game class

---
+++ milestone_4.py
@@ -0,0 +1,31 @@
+# %%
+import random
+
+class Hangman:
+	def __init__ (self, word_list, num_lives = 5):
+		self.word = random.choice(word_list)
+		self.word_guessed = ["_", "_", "_", "_", "_"]
+		self.num_letters = len(set(self.word))
+		self.num_lives = num_lives
+		self.word_list = word_list
+		self.list_of_guesses = []
+
+	def check_guess(self, guess):
+		guess = guess.lower()
+		if guess in self.word:
+			print("Good guess!", guess, " is in the word.")
+	
+	def ask_for_input(self):
+		while True:
+			print("Guess a single letter")
+			guess = input()
+			if len(guess) != 1 or not guess.isalpha():
+				print("Invalid letter. Please, enter a single alphabetical character.")
+			elif guess in self.list_of_guesses:
+				print("You already tried that letter!")
+			else:
+				self.check_guess(guess)
+				self.list_of_guesses.append(guess)
+	ask_for_input()
+
+# %%
