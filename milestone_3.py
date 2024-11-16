0ccfa001ac3e483b9d5115f33045afa4c1bc9530
Holly Morris <hollymorris@Hollys-MacBook-Air.local>
2024-11-16 14:09:23 +0000

    Enhances code readability

--- milestone_3.py
+++ milestone_3.py
@@ -4,24 +4,26 @@ import random
 names_of_favourite_fruit_list = ["mango", "blueberries", "strawberries", "banana", "pineapple"]
 name_of_favourite_fruit = random.choice(names_of_favourite_fruit_list)
 
-def check_guess(guess):
-	guess = guess.lower()
-	if guess in name_of_favourite_fruit:
-		print("Good guess!", guess, "is in the word.")
+def check_guess(guessed_letter):
+	guessed_letter = guessed_letter.lower()
+	if guessed_letter in name_of_favourite_fruit:
+		print("Good guess!", guessed_letter, "is in the word.")
 	else:
-		print("Sorry,", guess, " is not in the word. Try again.")
+		print("Sorry,", guessed_letter, " is not in the word. Try again.")
 
 def ask_for_input():
 	while True:
 		print("Guess a single letter:")
-		guess = input()
+		guessed_letter = input()
 	
-		if len(guess) == 1 and guess.isalpha():
+		if len(guessed_letter) == 1 and guessed_letter.isalpha():
 			break
 		else:
 			print("Invalid letter. Please, enter a single alphabetical character.")
 			continue 
-	check_guess(guess)
+	check_guess(guessed_letter)
 ask_for_input()
 
 
+
+# %%
